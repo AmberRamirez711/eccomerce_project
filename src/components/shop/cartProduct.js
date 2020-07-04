28b6020542549b56a8c9401e13bb61060fc58ec8
AmberRamirez711 <amberramirez@icloud.com>
2020-07-04 05:09:29 -0700

    cart product gris & alignment

--- src/components/shop/cartProduct.js
+++ src/components/shop/cartProduct.js
@@ -9,11 +9,11 @@ class CartProduct extends Component {
         const { title, price } = product;
         return (
             <div className={`${className} cart-product`}>
-                <img className='cart-product__image' src='http://via.placeholder.com/130x130'/>
+                <img className='cart-product__image' src='http://via.placeholder.com/130x130' />
                 <div className='cart-product__title'>{title}</div>
-                <Quantity className='cart-product__quantity' quantity={quantity}/>
+                <Quantity className='cart-product__quantity' quantity={quantity} />
                 <a className='cart-product__remove'>Remove</a>
-                <GreenPriceTag className='cart-product__price' title={price}/>
+                <GreenPriceTag className='cart-product__price' title={price} />
             </div>
         )
     }
