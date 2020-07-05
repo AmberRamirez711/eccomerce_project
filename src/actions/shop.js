74381b92ceb9c7481cb760ee22eecf95086b00e1
AmberRamirez711 <amberramirez@icloud.com>
2020-07-05 08:05:34 -0700

    implemented add to cart feature to add more cart products to a users cart

--- src/actions/shop.js
+++ src/actions/shop.js
@@ -14,7 +14,6 @@ export function filterProductsWithQuery(fields) {
 }
 
 export function filterProductsWithCategoryId(_id) {
-    console.log(_id);
     return ({
         type: FILTER_PRODUCTS_WITH_CATEGORY_ID,
         payload: _id
@@ -58,63 +57,57 @@ export function fetchShopCategories() {
     })
 }
 
+
 export function fetchShopProducts() {
     return ({
         type: SET_SHOP_PRODUCTS,
         payload: [
             {
                 _id: 0,
-                title: 'Javascript in the Browser',
-                description: 'When JavaScript was created, it initially had another name: “LiveScript”. But Java was very popular at that time, so it was decided that positioning a new language as a “younger brother” of Java would help.',
+                title: 'JavaScript in the Browser',
+                description: 'The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. ... The running speed starts slowly, but gets faster each minute after you hear this signal.',
                 price: 1.99,
                 belongsTo: [0, 1]
             },
             {
                 _id: 1,
                 title: 'Graph Database',
-                description: 'When JavaScript was created, it initially had another name: “LiveScript”. But Java was very popular at that time, so it was decided that positioning a new language as a “younger brother” of Java would help.',
+                description: 'The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. ... The running speed starts slowly, but gets faster each minute after you hear this signal.',
                 price: 1.99,
                 belongsTo: [0, 6]
             },
             {
                 _id: 2,
                 title: 'Full Stack Development',
-                description: 'When JavaScript was created, it initially had another name: “LiveScript”. But Java was very popular at that time, so it was decided that positioning a new language as a “younger brother” of Java would help.',
+                description: 'The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. ... The running speed starts slowly, but gets faster each minute after you hear this signal.',
                 price: 1.99,
                 belongsTo: [0, 1, 4]
             },
             {
                 _id: 3,
-                title: 'Javascript Development',
-                description: 'When JavaScript was created, it initially had another name: “LiveScript”. But Java was very popular at that time, so it was decided that positioning a new language as a “younger brother” of Java would help.',
+                title: 'User Interface Design',
+                description: 'The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. ... The running speed starts slowly, but gets faster each minute after you hear this signal.',
                 price: 1.99,
-                belongsTo: [0, 1]
+                belongsTo: [0, 2]
             },
             {
                 _id: 4,
-                title: 'User Interface Design',
-                description: 'When JavaScript was created, it initially had another name: “LiveScript”. But Java was very popular at that time, so it was decided that positioning a new language as a “younger brother” of Java would help.',
-                price: 1.99,
-                belongsTo: [0, 2]
-            },
-            {
-                _id: 5,
-                title: 'Javascript Development',
-                description: 'When JavaScript was created, it initially had another name: “LiveScript”. But Java was very popular at that time, so it was decided that positioning a new language as a “younger brother” of Java would help.',
+                title: 'JavaScript Development',
+                description: 'The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. ... The running speed starts slowly, but gets faster each minute after you hear this signal.',
                 price: 1.99,
                 belongsTo: [0, 1]
             },
             {
-                _id: 6,
+                _id: 5,
                 title: 'User Experience Design',
-                description: 'When JavaScript was created, it initially had another name: “LiveScript”. But Java was very popular at that time, so it was decided that positioning a new language as a “younger brother” of Java would help.',
+                description: 'The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. ... The running speed starts slowly, but gets faster each minute after you hear this signal.',
                 price: 1.99,
                 belongsTo: [0, 2]
             },
             {
-                _id: 0,
+                _id: 6,
                 title: 'Advanced OOP',
-                description: 'When JavaScript was created, it initially had another name: “LiveScript”. But Java was very popular at that time, so it was decided that positioning a new language as a “younger brother” of Java would help.',
+                description: 'The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. ... The running speed starts slowly, but gets faster each minute after you hear this signal.',
                 price: 1.99,
                 belongsTo: [0, 6]
             },
